68ea20956a83992df269cd571ddfd4e6384dd95c
Matt Richardson <mrichardson23@gmail.com>
2015-10-28 20:34:28 +0000

    lat long decimal conversion, adafruit IO

--- gps.py
+++ gps.py
@@ -1,4 +1,7 @@
 import serial
+import os
+from Adafruit_IO import Client
+import credentials
 
 ser = serial.Serial(
     port='/dev/ttyUSB0',\
@@ -9,6 +12,23 @@ ser = serial.Serial(
         timeout=1)
 
 print("connected to: " + ser.portstr)
+
+aio = Client(credentials.ADAFRUIT_IO_KEY)
+
+def degrees_to_decimal(data, hemisphere):
+    try:
+        decimalPointPosition = data.index('.')
+        degrees = float(data[:decimalPointPosition-2])
+        minutes = float(data[decimalPointPosition-2:])/60
+        print "degrees: " + str(degrees)
+        print "minutes: " + str(minutes)
+        output = degrees + minutes
+        if hemisphere is 'N' or hemisphere is 'E':
+            return output
+        if hemisphere is 'S' or hemisphere is 'W':
+            return -output
+    except:
+        return ""
 
 def parse_GPRMC(data):
     data = data.split(',')
@@ -26,6 +46,8 @@ def parse_GPRMC(data):
             'variation_e_w' : data[11],
             'checksum' : data[12]
     }
+    dict['decimal_latitude'] = degrees_to_decimal(dict['latitude'], dict['latitude_hemisphere'])
+    dict['decimal_longitude'] = degrees_to_decimal(dict['longitude'], dict['longitude_hemisphere'])
     return dict
 
 while True:
@@ -33,5 +55,15 @@ while True:
     if "$GPRMC" in line:
         gpsData = parse_GPRMC(line)
         if gpsData['validity'] == "A":
-            print "Lat: " + gpsData['latitude']+gpsData['latitude_hemisphere']
-            print "Long: " + gpsData['longitude']+gpsData['longitude_hemisphere']
+            #os.system('clear')
+            #print "Lat: " + gpsData['latitude']+gpsData['latitude_hemisphere']
+            #print "Long: " + gpsData['longitude']+gpsData['longitude_hemisphere']
+            with open("/home/pi/gps_experimentation/log.txt", "a") as myfile:
+                myfile.write(gpsData['fix_date'] + "," + gpsData['fix_time'] + "," + gpsData['latitude'] + "," + gpsData['latitude_hemisphere'] + "," + gpsData['longitude'] + "," + gpsData['longitude_hemisphere'] +"\n")
+            with open("/home/pi/gps_experimentation/raw_log.txt", "a") as myfile:
+                myfile.write(line)
+            payload = {'lat': gpsData['decimal_latitude'],
+                        'long': gpsData['decimal_longitude']
+            }
+            aio.send(payload)
+
